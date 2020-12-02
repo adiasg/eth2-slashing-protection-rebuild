cc591918028174ac21dcfab3c3f64a83357e89af
Aditya Asgaonkar <aditya.asgaonkar@gmail.com>
2020-12-02 00:09:12 -0800

    Added future_only method to rebuild

--- README.md
+++ README.md
@@ -1,11 +1,26 @@
 # Slashing Protection Recovery
 
-This repository contains a script to recover Eth2 validator slashing protection information in the form of the [Validator Client Interchange Format (Slashing Protection)](https://eips.ethereum.org/EIPS/eip-3076).
+This repository contains a script to recover Eth2 validator slashing protection information in the format defined by [EIP-3076: Validator Client Interchange Format (Slashing Protection)](https://eips.ethereum.org/EIPS/eip-3076).
 
 :bangbang:**Disclaimer**: *Absolutely no warranties. Use at your own risk.*:bangbang:
 
 ## Overview
-The script currently only regenerates the attestation component of the slashing protection file. The block protection component will be filled with something that prevents signing any blocks that are older than the previous slot. 
+
+### Slashing Protection Regeneration Methods
+#### `future_only` (Safe)
+The `future_only` method generates slashing protection information in a way that only allows the validator to make attestations in future epochs, and blocks in future slots.
+
+This method will make a single entry each in the attestation slashing protection and block slashing protection items. The items will be generated in the following manner:
+- Attestation Slashing Protection:
+  - `source_epoch`: Finalized epoch
+  - `target_epoch`: Current epoch
+- Block Slashing Protection:
+  - `slot`: Current slot
+
+This prevents the validator from making attestations in the current epoch, and blocks in the current slot, because of [slashing protection conditions 2, 4, and 5](https://eips.ethereum.org/EIPS/eip-3076#conditions). This may lead to some false positives from the slashing prevention detection component (i.e., messages that are not actually slashable are identified as slashable), but never leads to false negatives. **This means that the validator may possibly lose out on staking rewards for this epoch, but will definitely not experience a slashing penalty.**
+
+#### `parse_chain` (Experimental)
+The method currently only regenerates the attestation component of the slashing protection file. The block protection item will be filled with the current slot.
 
 The slashing protection information is generated in the following way:
 - Parse entire subtree descending from last finalized block
@@ -13,23 +28,37 @@ The slashing protection information is generated in the following way:
 - If no signed attestations are found in this subtree, make a single entry in the `"signed_attestations"` field of the slashing protection file with `"source_epoch"` and `"target_epoch"` set to the last finalized epoch.
 
 ### Assumptions
-The validator has always been using a validator client with accurate system time.
+- The validator is offline while running this script.
+- This script is run on a machine with accurate system time.
+- The validator has always been using a validator client with accurate system time.
+- There is only one finalized chain.
 
 ## Install
 
-- **Prerequisites**: Python 3
+- **Prerequisites**:
+  - Python 3
+  - A fully synced Eth2 Beacon Node with an accessible [Eth2 API](https://github.com/ethereum/eth2.0-APIs) endpoint
 - **Installation**: Run `make install`, then `. venv/bin/activate`
 
 ## Usage
 ```
-usage: python rebuild_slashing_protection.py [-h] --eth2-api ETH2_API --validator-index VALIDATOR_INDEX
-                                         [--log-level {debug,info,warn}]
+usage: rebuild_slashing_protection.py [-h] --eth2-api ETH2_API --method {future_only,parse_chain}
+                                      (--validator-index VALIDATOR_INDEX [VALIDATOR_INDEX ...] | --validator-pubkey VALIDATOR_PUBKEY [VALIDATOR_PUBKEY ...])
+                                      [--log-level {debug,info,warn}]
+
+Script to rebuild Eth2 validator slashing protection information
 
 optional arguments:
   -h, --help            show this help message and exit
   --eth2-api ETH2_API   Eth2 API to fetch Beacon Chain information from
-  --validator-index VALIDATOR_INDEX
-                        index of validator for which to regenerate slashing protection information
+  --method {future_only,parse_chain}
+                        method used to rebuild the slashing protection information. The "parse_chain" option is
+                        experimental, and must be used with the "--validator-index" argument with a single validator index
+                        input.
+  --validator-index VALIDATOR_INDEX [VALIDATOR_INDEX ...]
+                        index(es) of validator for which to regenerate slashing protection information
+  --validator-pubkey VALIDATOR_PUBKEY [VALIDATOR_PUBKEY ...]
+                        pubkey(s) of validator for which to regenerate slashing protection information
   --log-level {debug,info,warn}
                         preferred log level
 ```
--- rebuild_slashing_protection.py
+++ rebuild_slashing_protection.py
@@ -2,23 +2,35 @@ import argparse
 import httpx
 import json
 import logging
+import time
 from eth2spec.phase0.spec import (
-    SLOTS_PER_EPOCH,
+    SECONDS_PER_SLOT, SLOTS_PER_EPOCH,
     Attestation, BeaconState, SignedBeaconBlock,
     compute_epoch_at_slot, get_beacon_committee
 )
 
-parser = argparse.ArgumentParser()
+
+# Define command line options
+parser = argparse.ArgumentParser(description='Script to rebuild Eth2 validator slashing protection information')
 parser.add_argument("--eth2-api", type=str, required=True,
                     help="Eth2 API to fetch Beacon Chain information from")
-parser.add_argument("--validator-index", type=int, required=True,
-                    help="index of validator for which to regenerate "
+parser.add_argument("--method", type=str, required=True,
+                    choices=["future_only", "parse_chain"],
+                    help="method used to rebuild the slashing protection information. "
+                    'The "parse_chain" option is experimental, and must be used with the "--validator-index" argument with a single validator index input. ')
+val_input_group = parser.add_mutually_exclusive_group(required=True)
+val_input_group.add_argument("--validator-index", type=int, nargs='+',
+                    help="index(es) of validator for which to regenerate "
+                    "slashing protection information")
+val_input_group.add_argument("--validator-pubkey", type=str, nargs='+',
+                    help="pubkey(s) of validator for which to regenerate "
                     "slashing protection information")
 parser.add_argument("--log-level", type=str, required=False, default="info",
                     choices=["debug", "info", "warn"],
                     help="preferred log level")
 args = parser.parse_args()
 
+# Setup logging
 logging.basicConfig(format='%(asctime)s -- %(levelname)s -- %(message)s')
 log_level = args.log_level
 if log_level == "warn":
@@ -28,11 +40,20 @@ elif log_level == "info":
 else:
     logging.getLogger().setLevel(logging.DEBUG)
 
+# Initiaize variables using command line input
 ETH2_API = args.eth2_api
+METHOD = args.method
 VAL_INDEX = args.validator_index
+VAL_PUBKEY = args.validator_pubkey
+if METHOD == "parse_chain":
+    if VAL_PUBKEY:
+        parser.error('The "--method parse_chain" option can only be used with the "--validator_index" option')
+    elif len(VAL_INDEX) > 1:
+        parser.error('The "--method parse_chain" option can only be used with a single input for the "--validator_index" option')
 
 
 def query_eth2_api(endpoint):
+    # Function to get data from the specified endpoint of the Eth2 API
     url = ETH2_API + endpoint
     response = httpx.get(url, timeout=100)
     if response.status_code != 200:
@@ -43,6 +64,106 @@ def query_eth2_api(endpoint):
     response_json = response.json()
     response_data = response_json["data"]
     return response_data
+
+
+def generate_validator_protection_json(validator_pubkey, att_source_epoch, att_target_epoch, block_slot):
+    # Generate a validator protection information item in interchange format version 5
+    # https://eips.ethereum.org/EIPS/eip-3076
+    return {
+                "pubkey": validator_pubkey,
+                "signed_blocks": [
+                    {
+                        "slot": str(block_slot),
+                    }
+                ],
+                "signed_attestations": [
+                    {
+                        "source_epoch": str(att_source_epoch),
+                        "target_epoch": str(att_target_epoch)
+                    }
+                ]
+            }
+
+
+def write_protection_file(genesis_validators_root, validator_protection_info):
+    # Write the validator protection information items to a protection file in interchange format version 5
+    # https://eips.ethereum.org/EIPS/eip-3076
+    protection_file = "protection-interchange.json"
+    protection_data = []
+    interchange_json = {
+                         "metadata": {
+                                "interchange_format_version": "5",
+                                "genesis_validators_root": genesis_validators_root
+                            },
+                         "data": validator_protection_info
+                        }
+    logging.info(f'Writing to file: {protection_file}')
+    with open(protection_file, "w") as f:
+        f.write(json.dumps(interchange_json, indent=2))
+
+
+# Check that the Beacon Node serving the Eth2 API is fully synced
+syncing = query_eth2_api("/eth/v1/node/syncing")
+logging.debug(f'Got syncing status for Beacon Node at {ETH2_API} - Status: {syncing}')
+if syncing["is_syncing"]:
+    logging.critical(f'The Beacon Node serving Eth2 API at {ETH2_API} is not fully synced! Status: {syncing}\n'
+                      'Processing based on further information from this Beacon Node is unsafe. Exiting program.')
+    exit(1)
+
+# Fetch genesis information
+genesis = query_eth2_api("/eth/v1/beacon/genesis")
+genesis_validators_root = genesis["genesis_validators_root"]
+logging.info(f'Fetched Genesis information - genesis_validators_root: {genesis_validators_root}')
+if METHOD == "future_only":
+    # "future_only" is the safe method to generate protection information
+    if VAL_INDEX:
+        # If validator indexes were entered, fetch the corresponding pubkeys
+        validator_indexes = VAL_INDEX
+        validator_pubkeys = []
+        for validator_index in validator_indexes:
+            validator_info = query_eth2_api(f'/eth/v1/beacon/states/head/validators/{validator_index}')
+            validator_pubkey = validator_info["validator"]["pubkey"]
+            logging.info(f'Got Validator information for INDEX: {validator_index} - PUBKEY: {validator_pubkey}')
+            validator_pubkeys.append(validator_pubkey)
+    else:
+        validator_pubkeys = VAL_PUBKEY
+    logging.info(f'Rebuilding validator protection file using method "{METHOD}" for the folowing Validators - ')
+    for i in range(len(validator_pubkeys)):
+        logging.info(f'\tPUBKEY: {validator_pubkeys[i]}')
+    genesis_time = int(genesis["genesis_time"])
+    current_time = int(time.time())
+    current_slot = (current_time - genesis_time) // SECONDS_PER_SLOT
+    current_epoch = compute_epoch_at_slot(current_slot)
+    finality_checkpoints = query_eth2_api("/eth/v1/beacon/states/head/finality_checkpoints")
+    finalized_epoch = int(finality_checkpoints["finalized"]["epoch"])
+    logging.info(f'Current chain information - Slot: {current_slot}, Epoch: {current_epoch}, Finalized Epoch: {finalized_epoch}')
+
+    # Set the source and target epoch for attestation slashing protection to the finalized epoch
+    att_source_epoch = finalized_epoch
+    att_target_epoch = current_epoch
+    logging.info(f'Plugging in fake Attestation with Source Epoch: {att_source_epoch}, Target Epoch: {att_target_epoch}')
+    # Set the block slot for block slashing protection to the current slot
+    block_slot = current_slot
+    logging.info(f'Plugging in fake Block with Slot: {block_slot}')
+    validator_protection_info = []
+    for validator_pubkey in validator_pubkeys:
+        validator_protection_info.append(generate_validator_protection_json(validator_pubkey, att_source_epoch, att_target_epoch, block_slot))
+    write_protection_file(genesis_validators_root, validator_protection_info)
+    exit(0)
+
+
+# ------------------------------------------------------------------------------
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! CAUTION !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# This section is experimental code. Do not use for critical purposes.
+# ------------------------------------------------------------------------------
+
+logging.debug("User prompted for comfirmation to proceed.")
+user_proceed = input(f'!!! CAUTION: The "{METHOD}" method is experimental !!! Are you sure you want to proceed? Y/[N]: ')
+if user_proceed.lower() != 'y':
+    print(user_proceed.lower())
+    logging.info("Did not receive confirmation to proceed. Exiting program.")
+    exit(0)
+logging.info(f'User confirmed to proceed with "{METHOD}" method to rebuild validator protection file.')
 
 
 def fetch_state(state_id):
@@ -85,7 +206,7 @@ def get_signed_attestations(block, head_state, validator_index, committee_cache,
     return signed_attestations
 
 
-def process_chain(head, finalized_epoch, processed_blocks, best_attestations_in_chains):
+def process_chain(head, finalized_epoch, processed_blocks, best_attestations_in_chains, validator_index):
     logging.info(f"Processing chain from head: {head}")
     head_root = head["root"]
     # The below block header fetching might cause problems sometimes
@@ -104,7 +225,7 @@ def process_chain(head, finalized_epoch, processed_blocks, best_attestations_in_
     block = fetch_block(current_block_root)
     while current_block_root not in processed_blocks and block.slot > finalized_epoch * SLOTS_PER_EPOCH:
         logging.info(f'Processing Block - Slot: {block.slot}, Root: {current_block_root}')
-        signed_attestations = get_signed_attestations(block, head_state, VAL_INDEX, committee_cache, head)
+        signed_attestations = get_signed_attestations(block, head_state, validator_index, committee_cache, head)
         if signed_attestations:
             logging.debug(f'Signed attestations in this block: {signed_attestations}')
             candidate_attestations.extend(signed_attestations)
@@ -127,9 +248,11 @@ def process_chain(head, finalized_epoch, processed_blocks, best_attestations_in_
         logging.debug(f'Block - Slot: {block.slot}, Root: {current_block_root} is not higher than Finalized Slot: {finalized_epoch * SLOTS_PER_EPOCH}')
 
 
-validator_info = query_eth2_api(f'/eth/v1/beacon/states/head/validators/{VAL_INDEX}')
+validator_index = VAL_INDEX[0]
+validator_info = query_eth2_api(f'/eth/v1/beacon/states/head/validators/{validator_index}')
 validator_pubkey = validator_info["validator"]["pubkey"]
-logging.info(f'Rebuilding validator protection file for Validator - Index: {VAL_INDEX}, pubkey: {validator_pubkey}')
+logging.info(f'Rebuilding validator protection file using method "{METHOD}" for Validator - Index: {validator_index}, pubkey: {validator_pubkey}')
+
 finality_checkpoints_before = query_eth2_api("/eth/v1/beacon/states/head/finality_checkpoints")
 heads = query_eth2_api("/eth/v1/debug/beacon/heads")
 finality_checkpoints = query_eth2_api("/eth/v1/beacon/states/head/finality_checkpoints")
@@ -148,7 +271,7 @@ best_attestations_in_chains = []
 
 sorted(heads, key=lambda head: int(head["slot"]), reverse=True)
 for head in heads:
-    process_chain(head, finalized_epoch, processed_blocks, best_attestations_in_chains)
+    process_chain(head, finalized_epoch, processed_blocks, best_attestations_in_chains, validator_index)
     best_attestation = max(best_attestations_in_chains, key=lambda att: att.data.target.epoch)
     if compute_epoch_at_slot(best_attestation.data.slot) > compute_epoch_at_slot(int(head["slot"])):
         break
@@ -159,41 +282,13 @@ if best_attestations_in_chains:
 else:
     logging.info("No attestations from this validator were found in the sub-tree rooted at the last finalized block.")
     best_attestation = Attestation()
-    best_attestation.source.epoch = finalized_epoch
-    best_attestation.target.epoch = finalized_epoch
-    logging.info(f'Plugging in fake Attestation with Source Epoch: {finalized_epoch}, Target Epoch: {finalized_epoch}')
+    best_attestation.data.source.epoch = finalized_epoch
+    best_attestation.data.target.epoch = finalized_epoch
+    logging.info(f'Plugging in fake Attestation with Source Epoch: {best_attestation.data.source.epoch}, Target Epoch: {best_attestation.data.target.epoch}')
 
 logging.info("This program does not rebuild block protection history yet. The highest known head slot will be plugged into the block protection component.")
 best_block_slot = heads[0]["slot"]
 logging.info(f'Plugging in fake Block with Slot: {best_block_slot}')
 
-genesis = query_eth2_api("/eth/v1/beacon/genesis")
-genesis_validators_root = genesis["genesis_validators_root"]
-logging.debug(f'Fetched genesis_validators_root: {genesis_validators_root}')
-
-interchange_json = {
-                     "metadata": {
-                            "interchange_format_version": "5",
-                            "genesis_validators_root": genesis_validators_root
-                        },
-                     "data": [
-                            {
-                                "pubkey": validator_pubkey,
-                                "signed_blocks": [
-                                    {
-                                        "slot": str(best_block_slot),
-                                    }
-                                ],
-                                "signed_attestations": [
-                                    {
-                                        "source_epoch": str(best_attestation.data.source.epoch),
-                                        "target_epoch": str(best_attestation.data.target.epoch)
-                                    }
-                                ]
-                            }
-                        ]
-                    }
-
-logging.info("Writing to file: protection-interchange.json")
-with open("protection-interchange.json", "w") as f:
-    f.write(json.dumps(interchange_json))
+validator_protection_info = [generate_validator_protection_json(validator_pubkey, best_attestation.data.source.epoch, best_attestation.data.target.epoch, best_block_slot)]
+write_protection_file(genesis_validators_root, validator_protection_info)
