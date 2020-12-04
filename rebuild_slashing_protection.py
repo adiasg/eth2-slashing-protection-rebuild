0dff00cb9a30f455dc5df64d8f5063c4394c6136
Aditya Asgaonkar <aditya.asgaonkar@gmail.com>
2020-12-03 19:42:32 -0800

    Added safest protection level #3

--- rebuild_slashing_protection.py
+++ rebuild_slashing_protection.py
@@ -11,29 +11,50 @@ from eth2spec.phase0.spec import (
     compute_epoch_at_slot, get_beacon_committee
 )
 
-
 # Define command line options
-parser = argparse.ArgumentParser(description='Script to rebuild Eth2 validator slashing protection information')
-parser.add_argument("--eth2-api", type=str, required=True,
-                    help="Eth2 API to fetch Beacon Chain information from")
+parser = argparse.ArgumentParser(description='Utility to rebuild Eth2 '
+                                 'validator slashing protection information')
+chain_info_group = parser.add_mutually_exclusive_group(required=True)
+chain_info_group.add_argument("--eth2-api", type=str,
+                              help="Eth2 API to fetch Beacon Chain information"
+                              " from")
+chain_info_group.add_argument("--genesis-info", type=str,
+                              help='file containing genesis information to use'
+                                   ' in the absence of an Eth2 API. Can only '
+                                   'be used with the following options: '
+                                   '"--genesis-info GENESIS_INFO --method '
+                                   'uc_safe (--validator_pubkey '
+                                   'VALIDATOR_PUBKEY [VALIDATOR_PUBKEY ...] | '
+                                   '--validator-pubkey-file '
+                                   'VALIDATOR_PUBKEY_FILE"')
 parser.add_argument("--method", type=str, required=True,
-                    choices=["future_only", "parse_chain"],
-                    help="method used to rebuild the slashing protection information. "
-                    'The "parse_chain" option is experimental, and must be used with the "--validator-index" argument with a single validator index input.')
+                    choices=["uc_safe", "future_only", "parse_chain"],
+                    help='method used to rebuild the slashing protection '
+                    'information. The "parse_chain" option is experimental, '
+                    'and must be used with argument "--validator-index '
+                    'VALIDATOR_INDEX" (a single validator index input).')
 val_input_group = parser.add_mutually_exclusive_group(required=True)
 val_input_group.add_argument("--validator-index", type=int, nargs='+',
-                    help="index(es) of validator for which to regenerate "
-                    "slashing protection information")
+                             help="index(es) of validator(s) for which to "
+                             "regenerate slashing protection information")
 val_input_group.add_argument("--validator-pubkey", type=str, nargs='+',
-                    help="pubkey(s) of validator for which to regenerate "
-                    "slashing protection information")
+                             help="pubkey(s) of validator(s) for which to "
+                             "regenerate slashing protection information")
+val_input_group.add_argument("--validator-pubkey-file", type=str,
+                             help='file containing whitespace-separated '
+                             'pubkey(s) of validator(s) for which to '
+                             'regenerate slashing protection information. The '
+                             'default output file is "protection-file.json"')
+parser.add_argument("--output-file", type=str, required=False,
+                    default="protection-file.json",
+                    help="output file for slashing protection information")
 parser.add_argument("--log-level", type=str, required=False, default="info",
                     choices=["debug", "info", "warn"],
                     help="preferred log level")
 args = parser.parse_args()
 
 # Setup logging
-logging.basicConfig(format='%(asctime)s -- %(levelname)s -- %(message)s')
+logging.basicConfig(format='%(asctime)s -- %(levelname)8s -- %(message)s')
 log_level = args.log_level
 if log_level == "warn":
     logging.getLogger().setLevel(logging.WARNING)
@@ -44,14 +65,41 @@ else:
 
 # Initiaize variables using command line input
 ETH2_API = args.eth2_api
+GENESIS_INFO = args.genesis_info
 METHOD = args.method
 VAL_INDEX = args.validator_index
 VAL_PUBKEY = args.validator_pubkey
+VAL_PUBKEY_FILE = args.validator_pubkey_file
+OUTPUT_FILE = args.output_file
+
+if VAL_PUBKEY_FILE:
+    # Read the validator pubkey file and initiaize VAL_PUBKEY
+    with open(VAL_PUBKEY_FILE, "r") as val_pubkey_file:
+        file_content = val_pubkey_file.read()
+        logging.info(f'Fetching validator pubkey(s) from file: {GENESIS_INFO}')
+        VAL_PUBKEY = file_content.strip().split()
+        logging.info(f'Reading validator pubkeys from file: {VAL_PUBKEY_FILE}')
+        for i in range(len(VAL_PUBKEY)):
+            logging.info(f'\tPUBKEY: {VAL_PUBKEY[i]}')
+
+OFFLINE_MODE = False
+if ETH2_API is None:
+    if METHOD != "uc_safe" or not GENESIS_INFO or not VAL_PUBKEY:
+        parser.error('Argument "--eth2-api ETH2_API" can be missing only when '
+                     'using the following options: "--genesis-info '
+                     'GENESIS_INFO --method uc_safe (--validator_pubkey '
+                     'VALIDATOR_PUBKEY [VALIDATOR_PUBKEY ...] | '
+                     '--validator-pubkey-file VALIDATOR_PUBKEY_FILE"')
+    else:
+        OFFLINE_MODE = True
+
 if METHOD == "parse_chain":
-    if VAL_PUBKEY:
-        parser.error('The "--method parse_chain" option can only be used with the "--validator_index" option')
+    if not VAL_INDEX:
+        parser.error('The "--method parse_chain" argument can only be used '
+                     'with the "--validator_index" option')
     elif len(VAL_INDEX) > 1:
-        parser.error('The "--method parse_chain" option can only be used with a single input for the "--validator_index" option')
+        parser.error('The "--method parse_chain" option can only be used with '
+                     'a single index input for the "--validator_index" option')
 
 
 def query_eth2_api(endpoint):
@@ -90,7 +138,7 @@ def generate_validator_protection_json(validator_pubkey, att_source_epoch, att_t
 def write_protection_file(genesis_validators_root, validator_protection_info):
     # Write the validator protection information items to a protection file in interchange format version 5
     # https://github.com/ethereum/EIPs/blob/master/EIPS/eip-3076.md#json-schema
-    protection_file = "protection-file.json"
+    protection_file = OUTPUT_FILE
     interchange_json = {
                          "metadata": {
                                 "interchange_format_version": "5",
@@ -103,20 +151,37 @@ def write_protection_file(genesis_validators_root, validator_protection_info):
         f.write(json.dumps(interchange_json, indent=2))
 
 
-# Check that the Beacon Node serving the Eth2 API is fully synced
-syncing = query_eth2_api("/eth/v1/node/syncing")
-logging.debug(f'Got syncing status for Beacon Node at {ETH2_API} - Status: {syncing}')
-if syncing["is_syncing"]:
-    logging.critical(f'The Beacon Node serving Eth2 API at {ETH2_API} is not fully synced! Status: {syncing}\n'
-                      'Processing based on further information from this Beacon Node is unsafe. Exiting program.')
-    exit(1)
-
-# Fetch genesis information
-genesis = query_eth2_api("/eth/v1/beacon/genesis")
+# Fetch genesis information from appropriate source
+if OFFLINE_MODE:
+    # Fetch genesis information from genesis information file
+    with open(GENESIS_INFO, "r") as genesis_info_file:
+        file_content = genesis_info_file.read()
+        logging.info(f'Fetching Genesis information from file {GENESIS_INFO}')
+        genesis = json.loads(file_content)
+else:
+    # Check that the Beacon Node serving the Eth2 API is fully synced
+    syncing = query_eth2_api("/eth/v1/node/syncing")
+    logging.debug(f'Got syncing status for Beacon Node at {ETH2_API} - Status: {syncing}')
+    if syncing["is_syncing"]:
+        logging.critical(f'The Beacon Node serving Eth2 API at {ETH2_API} is not fully synced! Status: {syncing}\n'
+                          'Processing based on further information from this Beacon Node is unsafe. Exiting program.')
+        exit(1)
+    # Fetch genesis information from Eth2 API
+    logging.info(f'Fetching Genesis information from Eth2 API at {ETH2_API}')
+    genesis = query_eth2_api("/eth/v1/beacon/genesis")
 genesis_validators_root = genesis["genesis_validators_root"]
-logging.info(f'Fetched Genesis information - genesis_validators_root: {genesis_validators_root}')
-if METHOD == "future_only":
-    # "future_only" is the safe method to generate protection information
+logging.debug(f'Fetched Genesis information: {genesis}')
+
+if METHOD in ["uc_safe", "future_only"]:
+    genesis_time = int(genesis["genesis_time"])
+    current_time = int(time.time())
+    if current_time < genesis_time:
+        logging.critical(f'Current time is lesser than genesis time - Current time: {current_time}, Genesis time: {genesis_time}. Exiting program.')
+        exit(1)
+    current_slot = (current_time - genesis_time) // SECONDS_PER_SLOT
+    current_epoch = compute_epoch_at_slot(current_slot)
+    logging.info(f'Current chain information - Slot: {current_slot}, Epoch: {current_epoch}')
+
     if VAL_INDEX:
         # If validator indexes were entered, fetch the corresponding pubkeys
         validator_indexes = VAL_INDEX
@@ -131,20 +196,26 @@ if METHOD == "future_only":
     logging.info(f'Rebuilding validator protection file using method "{METHOD}" for the folowing Validators - ')
     for i in range(len(validator_pubkeys)):
         logging.info(f'\tPUBKEY: {validator_pubkeys[i]}')
-    genesis_time = int(genesis["genesis_time"])
-    current_time = int(time.time())
-    current_slot = (current_time - genesis_time) // SECONDS_PER_SLOT
-    current_epoch = compute_epoch_at_slot(current_slot)
-    finality_checkpoints = query_eth2_api("/eth/v1/beacon/states/head/finality_checkpoints")
-    justified_epoch = int(finality_checkpoints["current_justified"]["epoch"])
-    logging.info(f'Current chain information - Slot: {current_slot}, Epoch: {current_epoch}, Current Justified Epoch: {justified_epoch}')
 
-    # Set the source and target epoch for attestation slashing protection to the justified epoch
-    att_source_epoch = justified_epoch
-    att_target_epoch = current_epoch
+    if METHOD == "uc_safe":
+        # Set the source and target epoch for attestation slashing protection to the previous epoch and current epoch respectively
+        att_source_epoch = current_epoch - 1
+        att_target_epoch = current_epoch
+        # Set the block slot for block slashing protection to the current slot
+        block_slot = current_slot
+
+    if METHOD == "future_only":
+        # Fetch the current justified checkpoint information
+        finality_checkpoints = query_eth2_api("/eth/v1/beacon/states/head/finality_checkpoints")
+        justified_epoch = int(finality_checkpoints["current_justified"]["epoch"])
+        logging.info(f'Current justified checkpoint information - Current Justified Epoch: {justified_epoch}')
+        # Set the source and target epoch for attestation slashing protection to the justified epoch
+        att_source_epoch = justified_epoch
+        att_target_epoch = current_epoch
+        # Set the block slot for block slashing protection to the current slot
+        block_slot = current_slot
+
     logging.info(f'Plugging in fake Attestation with Source Epoch: {att_source_epoch}, Target Epoch: {att_target_epoch}')
-    # Set the block slot for block slashing protection to the current slot
-    block_slot = current_slot
     logging.info(f'Plugging in fake Block with Slot: {block_slot}')
     validator_protection_info = []
     for validator_pubkey in validator_pubkeys:
